23647d32f8635fc3c7d81dc5495c3e8463f7f408
NoePerezFab <noeperez@e-gisnet.com>
2022-03-09 23:09:57 -0600

    Cambio en cros origin

--- src/main/java/com/eventos/controller/EventoController.java
+++ src/main/java/com/eventos/controller/EventoController.java
@@ -8,12 +8,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class EventoController {
     private final int NUMERO_JUECES = 5;
     private ObjectMapper maper = new ObjectMapper();
