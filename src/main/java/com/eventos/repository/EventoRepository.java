a5149c6b8e83e81ab825f45b633370c1e9eb9f5f
NoePerezFab <noeperez@e-gisnet.com>
2022-07-07 12:26:31 -0500

    Generacion de reporte

--- src/main/java/com/eventos/repository/EventoRepository.java
+++ src/main/java/com/eventos/repository/EventoRepository.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Repository;
 public interface EventoRepository extends JpaRepository<Evento, Long>{
     Optional<Evento> findByEstado(int estado);
     
-    @Query("select e.nombre, r.nombre, c.nombre as Nombre, j.tipo as Tipo,"+
+    @Query(value ="select e.nombre, r.nombre, c.nombre as Nombre, j.tipo as Tipo,"+
         "max(ca.calificacion) filter (where j.nombre  = 'Juez 1') as Juez1, "+
 	"max(ca.calificacion) filter (where j.nombre  = 'Juez 2') as Juez2,"+
 	"max(ca.calificacion) filter (where j.nombre  = 'Juez 3') as Juez3,"+
@@ -21,6 +21,6 @@ public interface EventoRepository extends JpaRepository<Evento, Long>{
 	"max(ca.calificacion) filter (where j.nombre  = 'Juez 5') as Juez5 "+
 	"from  evento as e inner join rutina as r on e.id = r.evento_id  inner join competidor as c on r.id = c.rutina_id "+
 	"inner join calificacion as ca on c.id = ca.competidor_id inner join juez as j on ca.juez_id = j.id where e.id = :eid group by j.tipo,c.nombre,r.nombre, e.nombre "+
-	"order by c.nombre, j.tipo")
+	"order by c.nombre, j.tipo",nativeQuery=true)
     List<Object> findCustomEvento(@Param("eid")Long eid);
 }
